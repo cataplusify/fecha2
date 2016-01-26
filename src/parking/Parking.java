916459dc17659b1ab6c2634db6ab693c8a759cb5
203pc19 <203pc19@203pc15-PC>
2016-01-26 11:17:01 +0100

    2 version

---
+++ src/parking/Parking.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parking;
+
+/**
+ *
+ * @author 203pc19
+ */
+public class Parking {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NewClass p=new NewClass();
+        p.parking(10);
+        p.entrada();
+        p.salida(10);
+        p.visualizar();
+    }
+    public  void pruebastatic(){
+    
+    int prueba=0;
+    
+          
+}
+    
+}
